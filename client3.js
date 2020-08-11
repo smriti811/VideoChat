5fe584f3a9440b9bf6b043a7f59aa17df82d1663
smriti811 <69458853+smriti811@users.noreply.github.com>
2020-08-11 13:32:08 +0530

    Update client3.js

--- client3.js
+++ client3.js
@@ -298,10 +298,4 @@ function takepicture() {
         clearphoto();
     }
 }
-function ignoreFavicon(req, res, next) {
-  if (req.originalUrl === '/favicon.ico') {
-    res.status(204).json({nope: true});
-  } else {
-    next();
-  }
-}
+
