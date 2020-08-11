108cc0bafa7731e99052275604c7ca3252ff6b82
smriti811 <69458853+smriti811@users.noreply.github.com>
2020-08-11 18:46:04 +0530

    Update CallAjentClient.js

--- CallAjentClient.js
+++ CallAjentClient.js
@@ -196,7 +196,7 @@ function handleLogin(success) {
         }, function (error) {
             console.log(error);
         });
-          setTimeout(function () { document.getElementById("callBtn").click(); }, 3000);
+          setTimeout(function () { document.getElementById("callBtn").click(); }, 1000);
     }
 };
 
