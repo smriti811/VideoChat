38bc0ba70c9dd401497c6a796aacbbc92261ed56
smriti811 <69458853+smriti811@users.noreply.github.com>
2020-08-11 13:59:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 'use strict';
-//var http = require('http');
-var port = process.env.PORT || 1337;
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
 
-//http.createServer(function (req, res) {
-//    res.writeHead(200, { 'Content-Type': 'text/plain' });
-//    res.end('Hello World\n');
-//}).listen(port);
+app.use(express.static(__dirname + "/"))
 
-//require our websocket library 
-var WebSocketServer = require('ws').Server;
+var server = http.createServer(app)
+server.listen(port)
 
-//creating a websocket server at port 9090 
-var wss = new WebSocketServer({ port: 9090 });
+console.log("http server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
 
 //all connected to the server users 
 var users = {};
