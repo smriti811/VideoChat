307a56f355565d06748f8125da2a4d88f771bef7
smriti811 <69458853+smriti811@users.noreply.github.com>
2020-08-11 17:28:17 +0530

    Update client3.js

--- client3.js
+++ client3.js
@@ -4,7 +4,7 @@ var connectedUser;
 
 //connecting to our signaling server 
 //var conn = new WebSocket('wss://echo.websocket.org/');
-var conn = new WebSocket('ws://localhost:9090/');
+var conn = new WebSocket('wss://videochat8.herokuapp.com/');
 
 
 
